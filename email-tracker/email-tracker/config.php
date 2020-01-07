3ff6af45cdc7036028ca921e935544b307e87897
gautamGeekOMeter <59390089+gautamGeekOMeter@users.noreply.github.com>
2020-01-08 00:10:37 +0530

    Add files via upload

---
+++ email-tracker/email-tracker/config.php
@@ -0,0 +1,15 @@
+<?php
+
+	$mysql_hostname = 'localhost';
+	$mysql_username = 'root';
+	$mysql_password = '';
+	$mysql_dbname = 'email_track';
+	$protocol ='http';
+	$folder_path = 'email-tracker';
+	
+	
+	include "create-table.php";
+?>
+
+
+
